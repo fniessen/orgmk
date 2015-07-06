1577ab5008fd4244224d2cc1a73f2c0efb8f7a43
Sean Farley <sean@farley.io>
2015-07-06 14:54:25 -0700

    orgmk: refresh package archive contents if missing
    For new installations of emacs (such as, say, a docker container), the package
    contents are never initialized and htmlize cannot be installed. This patch
    checks the package-archive-contents variable and refreshes as needed.

--- site-lisp/orgmk.el
+++ site-lisp/orgmk.el
@@ -35,7 +35,9 @@
   (require 'package)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-  (package-initialize))
+  (package-initialize)
+  (unless package-archive-contents
+    (package-refresh-contents)))
 
 (unless (locate-library "ox")           ; trick to detect the presence of Org 8
   (ding)
